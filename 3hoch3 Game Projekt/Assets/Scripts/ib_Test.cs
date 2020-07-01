894c77871f91726625124d8f738d0f5e56a5867e
Tim Liehr <67097570+BrI4n99@users.noreply.github.com>
2020-07-01 18:24:34 +0200

    Erste Version
    Erste Version

---
+++ 3hoch3 Game Projekt/Assets/Scripts/ib_Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ib_Test : MonoBehaviour
+{
+
+    GameObject heuballen;
+    GameObject moehre;
+    GameObject kiste;
+    GameObject schaf;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start Test-Script Isabell Buerkner");
+
+        schaf = GameObject.CreatePrimitive(PrimitiveType.Cube); schaf.name = "schafRudi";
+        moehre = GameObject.CreatePrimitive(PrimitiveType.Sphere); moehre.name = "moehre";
+
+        moehre.transform.position +=  new Vector3(2, 0, 2f);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update Test-Script Isabell Buerkner");
+
+    }
+}
---
+++ 3hoch3 Game Projekt/Assets/Scripts/tl_sheep_script.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tl_sheep_script : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 sheepVelocity;
+    private Vector3 move;
+    private bool sheepOnGround;
+    public float sheepSpeed;
+    public float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+        controller.center = new Vector3(0f, 0.7f, 0.12f);
+        controller.radius = 0.64f;
+        controller.height = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sheepOnGround = controller.isGrounded;
+        if (sheepOnGround && sheepVelocity.y < 0)
+        {
+            sheepVelocity.y = 0f;
+        }
+
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * sheepSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        if (Input.GetButtonDown("Jump") && sheepOnGround)
+        {
+            sheepVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        sheepVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(sheepVelocity * Time.deltaTime);
+    }
+}
